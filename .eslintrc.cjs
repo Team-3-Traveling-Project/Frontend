63214ebac98ea17b2ad3574840fc1be26657ea7c
egg-silver <v6984@naver.com>
2023-09-16 15:11:41 +0900

    none eslint

--- .eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  env: { browser: true, es2020: true, node: true },
-  extends: ['eslint:recommended', 'plugin:react-hooks/recommended'],
-  parserOptions: { ecmaVersion: 2020, sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-  },
-};
--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <div>gkgkg</div>
+      <div></div>
     </>
   );
 }
